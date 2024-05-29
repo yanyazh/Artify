ca3e2796f65292f8d5c26cf2c68d223926eedcd7
yanyazh <yahnyazh@gmail.com>
2024-05-29 16:44:16 +0200

    Usuwanie konta użytkownika /api/user/

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
-from user.views import UserPostView, OwnUserDetailView, PublicUserDetailView
+from user.views import UserPostView, OwnUserDetailView, PublicUserDetailView, DeleteAccountView
 from django.urls import path
 
 urlpatterns = [
     path('posts/', UserPostView.as_view(), name='user_posts'),
     path('info/', OwnUserDetailView.as_view(), name='user_info'),
-    path('info/<int:user_id>/', PublicUserDetailView.as_view(), name='public-user-detail')
+    path('info/<int:user_id>/', PublicUserDetailView.as_view(), name='public-user-detail'),
+    path('delete/', DeleteAccountView.as_view(), name='delete_account'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,27 +1,23 @@
 from django.http.response import JsonResponse
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
 from post.models import Post
 from post.serializers import PostSerializer
 from .serializers import UserSerializer
-from rest_framework.exceptions import NotFound
-from rest_framework.permissions import AllowAny
-
 from .models import User
-# Create your views here.
 
 class UserPostView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
         posts = Post.objects.filter(user_id=request.user.id)
         serializer = PostSerializer(posts, many=True)
-
         return Response({'posts': serializer.data})
 
 class OwnUserDetailView(APIView):
     permission_classes = [IsAuthenticated]
-
     def get(self, request):
         user = request.user
         serializer = UserSerializer(user)
@@ -34,6 +30,13 @@ class PublicUserDetailView(APIView):
             user = User.objects.get(id=user_id)
         except User.DoesNotExist:
             raise NotFound("User not found")
-
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+class DeleteAccountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response({"detail": "User account deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
